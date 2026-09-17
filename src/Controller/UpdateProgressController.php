e710525157e6646ba1cb63fe8d4b0af6d54ddf33
Jörg Zülich <58428037+Akranor@users.noreply.github.com>
2026-09-17 11:52:36 +0200

    Autoconfigure update progress controller

--- src/Controller/UpdateProgressController.php
+++ src/Controller/UpdateProgressController.php
@@ -12,8 +12,10 @@ use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Attribute\AsController;
 use Throwable;
 
+#[AsController]
 final class UpdateProgressController
 {
     private const MAX_ERROR_DETAIL_LENGTH = 800;
