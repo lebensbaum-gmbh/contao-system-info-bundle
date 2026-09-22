9d36c6321ad766567ada07bd9b0888a7909b0207
Jörg Zülich <58428037+Akranor@users.noreply.github.com>
2026-09-22 09:33:19 +0200

    Add PHP CLI resolution exception

---
+++ src/Update/PhpCliResolutionException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lebensbaum\ContaoSystemInfoBundle\Update;
+
+use RuntimeException;
+
+final class PhpCliResolutionException extends RuntimeException
+{
+}
